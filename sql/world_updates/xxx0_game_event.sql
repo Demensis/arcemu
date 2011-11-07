63e5a593164baabe09ddc269c65e6a0bfbd3821c
Josua Mayer <josua.m@t-online.de>
2011-11-07 15:17:34 +0100

    * add missing files from last commit

---
+++ sql/world_updates/xxx0_game_event.sql
@@ -0,0 +1,99 @@
+DROP TABLE IF EXISTS `game_event`;
+CREATE TABLE `game_event` (
+  `event_id` int(10) unsigned NOT NULL,
+  `start_time` int(10) unsigned NOT NULL,
+  `length` int(10) unsigned NOT NULL,
+  `occurence` int(10) unsigned NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `game_event_creature_spawns`;
+CREATE TABLE `game_event_creature_spawns` (
+  `event_id` int(10) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry` int(30) NOT NULL,
+  `map` int(30) NOT NULL,
+  `position_x` float NOT NULL,
+  `position_y` float NOT NULL,
+  `position_z` float NOT NULL,
+  `orientation` float NOT NULL,
+  `movetype` int(30) NOT NULL DEFAULT '0',
+  `displayid` int(30) unsigned NOT NULL DEFAULT '0',
+  `faction` int(30) NOT NULL DEFAULT '14',
+  `flags` int(30) NOT NULL DEFAULT '0',
+  `bytes0` int(30) NOT NULL DEFAULT '0',
+  `bytes1` int(30) NOT NULL DEFAULT '0',
+  `bytes2` int(30) NOT NULL DEFAULT '0',
+  `emote_state` int(30) NOT NULL DEFAULT '0',
+  `npc_respawn_link` int(30) NOT NULL DEFAULT '0',
+  `channel_spell` int(30) NOT NULL DEFAULT '0',
+  `channel_target_sqlid` int(30) NOT NULL DEFAULT '0',
+  `channel_target_sqlid_creature` int(30) NOT NULL DEFAULT '0',
+  `standstate` int(10) NOT NULL DEFAULT '0',
+  `death_state` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `mountdisplayid` int(10) unsigned NOT NULL DEFAULT '0',
+  `slot1item` int(10) unsigned NOT NULL DEFAULT '0',
+  `slot2item` int(10) unsigned NOT NULL DEFAULT '0',
+  `slot3item` int(10) unsigned NOT NULL DEFAULT '0',
+  `CanFly` smallint(3) NOT NULL DEFAULT '0',
+  `phase` int(10) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`event_id`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Creature Spawns for Events';
+
+DROP TABLE IF EXISTS `game_event_gameobject_spawns`;
+CREATE TABLE `game_event_gameobject_spawns` (
+  `event_id` int(10) unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Entry` int(10) unsigned NOT NULL DEFAULT '0',
+  `map` int(10) unsigned NOT NULL DEFAULT '0',
+  `position_x` float NOT NULL DEFAULT '0',
+  `position_y` float NOT NULL DEFAULT '0',
+  `position_z` float NOT NULL DEFAULT '0',
+  `Facing` float NOT NULL DEFAULT '0',
+  `orientation1` float NOT NULL DEFAULT '0',
+  `orientation2` float NOT NULL DEFAULT '0',
+  `orientation3` float NOT NULL DEFAULT '0',
+  `orientation4` float NOT NULL DEFAULT '0',
+  `State` int(10) unsigned NOT NULL DEFAULT '0',
+  `Flags` int(10) unsigned NOT NULL DEFAULT '0',
+  `Faction` int(10) unsigned NOT NULL DEFAULT '0',
+  `Scale` float NOT NULL DEFAULT '0',
+  `stateNpcLink` int(10) unsigned NOT NULL DEFAULT '0',
+  `phase` int(10) unsigned NOT NULL DEFAULT '1',
+  `overrides` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`event_id`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='GameObject Spawns for Events';
+
+DROP TABLE IF EXISTS `game_event_creature_waypoints`;
+CREATE TABLE `game_event_creature_waypoints` (
+  `event_id` int(10) unsigned NOT NULL,
+  `spawnid` int(10) unsigned NOT NULL DEFAULT '0',
+  `waypointid` int(10) unsigned NOT NULL DEFAULT '0',
+  `position_x` float NOT NULL DEFAULT '0',
+  `position_y` float NOT NULL DEFAULT '0',
+  `position_z` float NOT NULL DEFAULT '0',
+  `waittime` int(10) unsigned NOT NULL DEFAULT '0',
+  `flags` int(10) unsigned NOT NULL DEFAULT '0',
+  `forwardemoteoneshot` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `forwardemoteid` int(10) unsigned NOT NULL DEFAULT '0',
+  `backwardemoteoneshot` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `backwardemoteid` int(10) unsigned NOT NULL DEFAULT '0',
+  `forwardskinid` int(10) unsigned NOT NULL DEFAULT '0',
+  `backwardskinid` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`event_id`,`spawnid`,`waypointid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Creature Waypoints for Events';
+
+DROP TABLE IF EXISTS `game_event_scripts`;
+CREATE TABLE `game_event_scripts` (
+  `event_id` int(10) unsigned NOT NULL,
+  `sql_id` int(10) unsigned NOT NULL,
+  `type` tinyint(3) unsigned NOT NULL,
+  `data_1` int(10) unsigned NOT NULL,
+  `data_2` int(10) unsigned NOT NULL,
+  `data_3` int(10) unsigned NOT NULL,
+  `say_on_event_start` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `event_id` (`event_id`,`sql_id`,`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `quests` ADD `event_id` INT UNSIGNED NOT NULL DEFAULT '0';
---
+++ src/arcemu-world/GameEventManager.h
@@ -0,0 +1,119 @@
+// Own GameEvent Manager class
+// Copyright (C) 2011 HellGate-WoW
+
+#ifndef __GAMEVENTMGR_H
+#define __GAMEVENTMGR_H
+
+struct EventInfo
+{
+	// these are loaded from DB
+	uint32 start_time;	// UNIX time of the next event start
+	uint32 length;		// length in minutes!!	
+	uint32 occurence;	// 0 to not repeat event, otherwise in minutes
+	char* name;
+
+	// this is calculated or used in core only
+	uint32 end_time;
+	bool active;
+};
+
+// used for backuping original values
+struct EventScript
+{
+	// these are loaded from DB
+	uint32 sql_id;
+	uint8 type;
+	uint32 data_1;
+	uint32 data_2;
+	uint32 data_3;
+	uint32 mapid;
+};
+
+enum GamEventScriptTypes
+{
+	CREATURE_CHANGE_SCRIPTED_CHANGE = 0,
+	CREATURE_CHANGE_EMOTE,
+	CREATURE_CHANGE_DISPLAYID,
+	CREATURE_CHANGE_WEAPON,
+	CREATURE_CHANGE_REACT,
+	CREATURE_CAST_SPELL_ON_EVENT_START,
+	CREATURE_CAST_SPELL_ON_EVENT_STOP,
+	CREATURE_CHANGE_UPDATE_FIELD,
+	CREATURE_CHANGE_DESPAWN,
+};
+
+typedef std::map<uint32, EventInfo*> GameEventMap;
+typedef std::map<uint32, std::map<uint32, std::list<WayPoint *>>> GameEventWaypointMap;
+typedef std::map<uint32, std::map<uint32, std::list<uint32>>> GameEventSpawnMap;
+typedef std::map<uint32, set<EventScript*>> GameEventScriptMap;
+
+class SERVER_DECL GameEventMgr :  public Singleton < GameEventMgr >
+{
+private:
+
+	// private storages
+	GameEventMap m_GameEventMap; // contains basic info about all events
+	GameEventWaypointMap m_creaturewaypoints;
+	GameEventSpawnMap m_creaturespawns;
+	GameEventSpawnMap m_gameobjectspawns;
+	GameEventScriptMap m_GameEventScriptBackup;
+
+public:
+
+	// constructor & destructor
+	GameEventMgr();
+	~GameEventMgr();
+
+	// we are going to check all events by this
+	void CheckForEvents();
+
+	// method for loading all events
+	void LoadEvents();
+	void LoadEventsProc(QueryResultVector & results);
+
+	// saves event #id to  the database
+	bool SaveEvent(uint32 id);
+
+	// Activate and Deactivate events
+	void StartEvent(uint32 id);
+	void FinishEvent(uint32 id);
+
+	// Some check methods, which can be used everywhere...
+	bool IsEventActive(uint32 id)
+	{
+		GameEventMap::iterator itr = CheckAndReturnEvent( id );
+		if( itr == m_GameEventMap.end() )
+			return false;
+		else if( itr->second->active == false )
+			return false;
+		else
+			return true;
+	}
+
+private:
+	// This will load all objects and scripts from database (for given event id)
+	void ProcessObjectsAndScripts(uint32 id);
+	void ProcessObjectsAndScriptsProc(QueryResultVector & results, uint32 id);
+
+	// methods for spawning and despawning
+	void SpawnEvent(uint32 id);
+	void DespawnEvent(uint32 id);
+
+	void DoScript(uint32 event_id, uint32 sql_id, uint8 type, uint32 data1, uint32 data2, uint32 data3, char * say, uint32 mapid);
+	void ScriptedBackToOrig(uint32 event_id);
+
+	// Some needed functions
+	ARCEMU_INLINE GameEventMap::iterator CheckAndReturnEvent(uint32 id)
+	{
+		GameEventMap::iterator _event = m_GameEventMap.find(id);
+		if( _event == m_GameEventMap.end() )
+			return m_GameEventMap.end();
+		else
+			return _event;
+	}
+
+};
+
+#define sGameEventMgr GameEventMgr::getSingleton()
+
+#endif
