380c588e2c16808a2d19f7d840f78434c4a66a84
MesoX <MesoX@seznam.cz>
2011-11-20 20:50:33 +0100

    * Begin work on ebon hold script * Currently working on script for The Endless Hunger (#12848) and seems it's almost finished

--- src/scripts/src/QuestScripts/Quest_DeathKnight.cpp
+++ src/scripts/src/QuestScripts/Quest_DeathKnight.cpp
@@ -25,8 +25,13 @@ class ScourgeGryphonOne : public GossipScript
 	public:
 		void GossipHello(Object* pObject, Player* plr)
 		{
-			TaxiPath* path = sTaxiMgr.GetTaxiPath(1053);
-			plr->TaxiStart(path, 26308, 0);
+			if(plr->GetQuestLogForEntry(12670) ||  plr->HasFinishedQuest(12670))
+			{
+				TaxiPath* path = sTaxiMgr.GetTaxiPath(1053);
+				plr->TaxiStart(path, 26308, 0);
+				// remove Dominion Over Acherus, no need to check if target has this aura, actual check is done befor removing
+				plr->RemoveAllAuraById(51721);
+			}
 		}
 };
 
@@ -35,59 +40,271 @@ class ScourgeGryphonTwo : public GossipScript
 	public:
 		void GossipHello(Object* pObject, Player* plr)
 		{
-			TaxiPath* path = sTaxiMgr.GetTaxiPath(1054);
-			plr->TaxiStart(path, 26308, 0);
+			if(plr->GetQuestLogForEntry(12670) ||  plr->HasFinishedQuest(12670))
+			{
+				TaxiPath* path = sTaxiMgr.GetTaxiPath(1054);
+				plr->TaxiStart(path, 26308, 0);
+			}
 		}
 };
+
+// The Endless Hunger script for both GO and Unworthy Initiate
 
 #define CN_INITIATE_1				29519
 #define CN_INITIATE_2				29565
 #define CN_INITIATE_3				29567
 #define CN_INITIATE_4				29520
+#define CN_INITIATE_5				29566
+
+    //SPELL_CHAINED_PESANT_LH        54602,              // not used. possible it determine side, where to go get "weapon"
+    //SPELL_CHAINED_PESANT_RH        54610,
+#define    SPELL_CHAINED_PESANT_CHEST      54612
+#define    SPELL_CHAINED_PESANT_BREATH     54613
+#define    SPELL_INITIATE_VISUAL           51519
+
+#define    SPELL_BLOOD_STRIKE              52374
+#define    SPELL_DEATH_COIL                52375
+#define    SPELL_ICY_TOUCH                 52372
+#define    SPELL_PLAGUE_STRIKE             52373
+
+#define    NPC_ANCHOR                      29521
+#define    FACTION_MONSTER                 16
+
+#define    PHASE_INACTIVE                  0
+#define    PHASE_STANDUP                   1
+#define    PHASE_REACH_ARMORY              2
+#define    PHASE_ACTIVATE                  3
+#define    PHASE_ATTACK_PLAYER             4
+
+struct DisplayToSpell
+{
+	uint32 displayid;
+	uint32 spellid;
+};
+
+DisplayToSpell m_DisplayToSpell[] =
+{
+    {25354, 51520},                                         // human M
+    {25355, 51534},                                         // human F
+    {25356, 51538},                                         // dwarf M
+    {25357, 51541},                                         // draenei M
+    {25358, 51535},                                         // nelf M
+    {25359, 51539},                                         // gnome M
+    {25360, 51536},                                         // nelf F
+    {25361, 51537},                                         // dwarf F
+    {25362, 51540},                                         // gnome F
+    {25363, 51542},                                         // draenei F
+    {25364, 51543},                                         // orc M
+    {25365, 51546},                                         // troll M
+    {25366, 51547},                                         // tauren M
+    {25367, 51549},                                         // forsaken M
+    {25368, 51544},                                         // orc F
+    {25369, 51552},                                         // belf F
+    {25370, 51545},                                         // troll F
+    {25371, 51548},                                         // tauren F
+    {25372, 51550},                                         // forsaken F
+    {25373, 51551}                                          // belf M
+};
+
+static Location m_ArmoryLocations[] = 
+{
+	{ 2461.047363f, -5570.428223f, 415.664490f, 1.735095f },
+	{ 2472.961670f, -5575.300781f, 415.429169f, 0.368502f },
+	{ 2474.302246f, -5576.693848f, 415.394287f, 1.012529f },
+	{ 2480.435547f, -5584.607422f, 415.663696f, 0.328443f },
+	{ 2482.882324f, -5590.556152f, 415.663605f, 0.383420f },
+	{ 2448.824219f, -5612.786621f, 415.664154f, 3.540719f },
+	{ 2447.006104f, -5611.062500f, 415.664154f, 4.298627f },
+	{ 2440.834229f, -5604.432617f, 415.664154f, 4.243647f },
+	{ 2437.441895f, -5590.024414f, 415.663879f, 2.822077f },
+	{ 2439.216797f, -5585.857422f, 415.663879f, 2.802442f },
+};
 
 class AcherusSoulPrison : GameObjectAIScript
 {
 	public:
-		AcherusSoulPrison(GameObject* goinstance) : GameObjectAIScript(goinstance) {}
-		static GameObjectAIScript* Create(GameObject* GO)
+		AcherusSoulPrison(GameObject * goinstance) : GameObjectAIScript(goinstance) {}
+		static GameObjectAIScript * Create(GameObject* GO)
 		{
 			return new AcherusSoulPrison(GO);
 		}
 
 		void OnActivate(Player* pPlayer)
 		{
-			QuestLogEntry* en = pPlayer->GetQuestLogForEntry(12848);
-			if(!en)
+			QuestLogEntry * en = pPlayer->GetQuestLogForEntry( 12848 );
+			if( !en || en->GetMobCount(0) != 0 )
 				return;
 
 			float SSX = pPlayer->GetPositionX();
 			float SSY = pPlayer->GetPositionY();
 			float SSZ = pPlayer->GetPositionZ();
 
-			Creature* pCreature = pPlayer->GetMapMgr()->GetInterface()->GetCreatureNearestCoords(SSX, SSY, SSZ);
+			Creature * pCreature = pPlayer->GetMapMgr()->GetInterface()->GetCreatureNearestCoords(SSX, SSY, SSZ);
 
 			if(!pCreature || !pCreature->isAlive())
 				return;
 
-			if(pCreature->GetEntry() == CN_INITIATE_1 || pCreature->GetEntry() == CN_INITIATE_2 || pCreature->GetEntry() == CN_INITIATE_3 || pCreature->GetEntry() == CN_INITIATE_4)
+			if(pCreature->GetEntry() == CN_INITIATE_1 || 
+			   pCreature->GetEntry() == CN_INITIATE_2 ||
+			   pCreature->GetEntry() == CN_INITIATE_3 || 
+			   pCreature->GetEntry() == CN_INITIATE_4 ||
+			   pCreature->GetEntry() == CN_INITIATE_5 )
 			{
-				pPlayer->SendChatMessage(CHAT_MSG_SAY, LANG_UNIVERSAL, "I give you the key to your salvation");
-				pCreature->SetUInt64Value(UNIT_FIELD_FLAGS, 0);
-				pCreature->GetAIInterface()->setNextTarget(pPlayer);
-				pCreature->GetAIInterface()->AttackReaction(pPlayer, 1, 0);
-				pCreature->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, "You have committed a big mistake, demon");
+				pCreature->SetStandState(uint8(STANDSTATE_STAND));
+				float Facing = pCreature->calcRadAngle(pCreature->GetPositionX(), pCreature->GetPositionY(), pPlayer->GetPositionX(), pPlayer->GetPositionY());
+				pCreature->SetFacing(Facing);
 
-				if(en->GetMobCount(0) != 0)
-					return;
+				// this way we have refference to our player
+				pCreature->m_escorter = pPlayer;
 
-				en->SetMobCount(0, 1);
-				en->SendUpdateAddKill(0);
-				en->UpdatePlayerFields();
+				// do all other things directly in creature's AI class, it's more comfortable
+				pCreature->GetScript()->RegisterAIUpdateEvent(1000);
 			}
-
 		}
 };
 
+class UnworthyInitiate : public MoonScriptCreatureAI
+{
+	public:
+		MOONSCRIPT_FACTORY_FUNCTION(UnworthyInitiate, MoonScriptCreatureAI);
+		UnworthyInitiate(Creature * c) : MoonScriptCreatureAI(c)
+		{
+			AddSpell(SPELL_BLOOD_STRIKE, Target_Current, 75.0f, 0.0f, 4000);
+			AddSpell(SPELL_DEATH_COIL, Target_Current, 75.f, 0.0f, 6000);
+			AddSpell(SPELL_ICY_TOUCH, Target_Current, 75.f, 0.0f, 2000);
+			AddSpell(SPELL_PLAGUE_STRIKE, Target_Current, 75.f, 0.0f, 5000);
+			state = PHASE_INACTIVE;
+		}
+
+		void OnLoad()
+		{
+			// attach nearest prison and cast spell on it
+			float SSX = _unit->GetPositionX();
+			float SSY = _unit->GetPositionY();
+			float SSZ = _unit->GetPositionZ();
+			anchor = _unit->GetMapMgr()->GetInterface()->GetCreatureNearestCoords(SSX, SSY, SSZ, NPC_ANCHOR);
+			if( anchor )
+			{
+				anchor->CastSpell(_unit, SPELL_CHAINED_PESANT_BREATH, false);
+				_unit->CastSpell(_unit, SPELL_CHAINED_PESANT_CHEST, false);
+			}
+
+			ParentClass::OnLoad();
+		}
+
+		void AIUpdate()
+		{
+			if( state == PHASE_INACTIVE )
+			{
+				timer = AddTimer(1000);
+				state = PHASE_STANDUP;
+			}
+			else if( state == PHASE_STANDUP && IsTimerFinished(timer) )
+			{
+				_unit->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, "You have committed a big mistake, demon");
+				timer = AddTimer(5000);
+				state = PHASE_REACH_ARMORY;
+			}
+			else if( state == PHASE_REACH_ARMORY && IsTimerFinished(timer) )
+			{
+				// test for nearest coords of armory
+				uint32 nearest_entry = 0;
+				uint32 nearest_distance = 1000;
+				for(uint32 i = 0; i < sizeof(m_ArmoryLocations) / sizeof(Location); i++)
+				{
+					uint32 distance = _unit->CalcDistance(m_ArmoryLocations[i].x, m_ArmoryLocations[i].y, m_ArmoryLocations[i].z);
+
+					if( distance < nearest_distance )
+					{
+						nearest_distance = distance;
+						nearest_entry = i;
+					}
+				}
+				
+				// now go to nearest armory
+				MoveTo(m_ArmoryLocations[nearest_entry].x, m_ArmoryLocations[nearest_entry].y, m_ArmoryLocations[nearest_entry].z, true);
+				timer = AddTimer(2000);
+				state = PHASE_ACTIVATE;
+			}
+			else if( state == PHASE_ACTIVATE && IsTimerFinished(timer) )
+			{
+				// face off the player
+				float Facing = _unit->calcRadAngle(_unit->GetPositionX(), _unit->GetPositionY(), _unit->m_escorter->GetPositionX(), _unit->m_escorter->GetPositionY());
+				_unit->SetFacing(Facing);
+
+				// select suitable spell
+				uint32 spell_to_cast = 0;
+				for(uint32 i = 0; i < sizeof(m_DisplayToSpell) / sizeof(DisplayToSpell); i++)
+				{
+					if( m_DisplayToSpell[i].displayid == _unit->GetDisplayId() )
+					{
+						spell_to_cast = m_DisplayToSpell[i].spellid;
+						break;
+					}
+				}
+
+				// change look
+				_unit->CastSpell(_unit, spell_to_cast, false);
+				_unit->CastSpell(_unit, 48266, false); // blood presence
+				timer = AddTimer(2000);
+				state = PHASE_ATTACK_PLAYER;
+			}
+			else if( state == PHASE_ATTACK_PLAYER && IsTimerFinished(timer) )
+			{
+				_unit->SendChatMessage(CHAT_MSG_MONSTER_SAY, LANG_UNIVERSAL, "And now you die!");
+				state = -1; // we are fighting
+				timer = AddTimer(1000);
+			}
+			else if( state == -1 && IsTimerFinished(timer) )
+			{
+				_unit->SetUInt32Value(UNIT_FIELD_FLAGS, 0);
+				_unit->GetAIInterface()->setNextTarget(_unit->m_escorter);
+				_unit->GetAIInterface()->AttackReaction(_unit->m_escorter, 1, 0);
+			}
+
+			ParentClass::AIUpdate();
+		}
+
+		void OnDied(Unit * mKiller)
+		{
+			if( !mKiller->IsPlayer() )
+				return;
+
+			// don't credit if any other player kills my initiate
+			if( mKiller->GetGUID() != _unit->m_escorter->GetGUID() )
+				return;
+
+			Player * plr = TO_PLAYER(mKiller);
+			QuestLogEntry * qle = plr->GetQuestLogForEntry(12848);
+			if( !qle )
+				return;
+
+			qle->SetMobCount(0, 1);
+			qle->SendUpdateAddKill(0);
+			qle->UpdatePlayerFields();
+			qle->SendQuestComplete();
+
+			ParentClass::OnDied(mKiller);
+		}
+
+		void OnCombatStop(Unit * pTarget)
+		{
+			MoveToSpawnOrigin();
+			_unit->SetUInt32Value(UNIT_FIELD_FLAGS, 33024);
+			if( anchor )
+			{
+				anchor->CastSpell(_unit, SPELL_CHAINED_PESANT_BREATH, false);
+				_unit->CastSpell(_unit, SPELL_CHAINED_PESANT_CHEST, false);
+			}
+			_unit->SetStandState(uint8(STANDSTATE_KNEEL));
+			state = PHASE_INACTIVE;
+			ParentClass::OnCombatStop(pTarget);
+		}
+
+		int32 timer;
+		int8 state;
+		Creature * anchor;
+};
 
 
 void SetupDeathKnight(ScriptMgr* mgr)
@@ -110,4 +327,9 @@ void SetupDeathKnight(ScriptMgr* mgr)
 	mgr->register_gameobject_script(191589, &AcherusSoulPrison::Create);
 	mgr->register_gameobject_script(191590, &AcherusSoulPrison::Create);
 
+	mgr->register_creature_script(CN_INITIATE_1, &UnworthyInitiate::Create);
+	mgr->register_creature_script(CN_INITIATE_2, &UnworthyInitiate::Create);
+	mgr->register_creature_script(CN_INITIATE_3, &UnworthyInitiate::Create);
+	mgr->register_creature_script(CN_INITIATE_4, &UnworthyInitiate::Create);
+	mgr->register_creature_script(CN_INITIATE_5, &UnworthyInitiate::Create);
 }
